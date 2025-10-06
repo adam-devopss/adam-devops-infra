fe12d9b748fc75983fc4e365d981bc5a80d9120b
Adam DevOps <albakov.adam2015@gmail.com>
2025-10-06 17:53:03 +0300

    chore: lock YC provider 0.120.0 (local mirror)

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_providers {
     yandex = {
-      source  = "yandex-cloud/yandex"
-      version = "~> 0.120"
+      source  = "registry.opentofu.org/yandex-cloud/yandex"  
+      version = "0.120.0"
     }
   }
 }
